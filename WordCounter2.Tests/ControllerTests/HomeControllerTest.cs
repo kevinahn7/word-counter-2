eac8b910a53584ffa1d53cf67ec82d4057a9e106
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-06 10:01:29 -0700

    Add initial commit

---
+++ WordCounter2.Tests/ControllerTests/HomeControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCounter2.Tests
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ WordCounter2.Tests/ControllerTests/ResultsControllerTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCounter2.Tests
+{
+    [TestClass]
+    public class ResultsControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
