b307f12457b796b09d140f934390f3a272f861f1
Kevin Ahn <kevinahn7@hotmail.com>
2018-07-06 10:57:50 -0700

    Add test for setting array of the words

--- WordCounter2.Tests/ModelTests/RepeatCounter.Tests.cs
+++ WordCounter2.Tests/ModelTests/RepeatCounter.Tests.cs
@@ -13,5 +13,15 @@ namespace WordCounter2.Tests
             string word = test.GetTheWord();
             Assert.AreEqual(word, "one");
         }
+
+        [TestMethod]
+        public void GetSetPhrase_GetsSetsPhrase_ReturnEqualValue()
+        {
+            RepeatCounter test = new RepeatCounter("one", "one two three");
+            string[] phrase = test.GetTheArrayOfWords();
+            string testPhrase = "one two three";
+            string[] testPhraseArray = testPhrase.Split(' ');
+            CollectionAssert.AreEqual(phrase, testPhraseArray);
+        }
     }
 }
